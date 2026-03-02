fb8edf9c109757c30e2c1d4b00e191fec579d12b
nishanthveemarasan <iamnishanthveema@gmail.com>
2026-03-02 14:46:44 +0000

    Update form components to handle empty values and add basePath and assetPrefix in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+  basePath: '/next',
+  assetPrefix: '/next',
 };
 
 export default nextConfig;
